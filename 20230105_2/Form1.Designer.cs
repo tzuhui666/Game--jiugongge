ab070f78fccb36cf08be2481cb239b13d6ea4ecc
1102056洪慈惠 <96213059+tzuhui666@users.noreply.github.com>
2024-04-22 14:43:37 +0800

    Add files via upload

---
+++ 20230105_2/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace _20230105_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(215, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 58);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(340, 137);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(58, 58);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(93, 204);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(58, 58);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(215, 203);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(58, 58);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(340, 202);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(58, 58);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(93, 266);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(58, 58);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(215, 265);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(58, 58);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(340, 264);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(58, 58);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(137, 362);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(90, 42);
+            this.btnStart.TabIndex = 9;
+            this.btnStart.Text = "開始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(265, 362);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(90, 42);
+            this.btnEnd.TabIndex = 10;
+            this.btnEnd.Text = "結束";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTime.Location = new System.Drawing.Point(546, 176);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(100, 23);
+            this.lblTime.TabIndex = 11;
+            this.lblTime.Text = "秒數";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblCount
+            // 
+            this.lblCount.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCount.Location = new System.Drawing.Point(546, 242);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(100, 23);
+            this.lblCount.TabIndex = 12;
+            this.lblCount.Text = "次數";
+            this.lblCount.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "九宮格益智遊戲";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 20230105_2/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20230105_2
+{
+    public partial class Form1 : Form
+    {
+        Color onColor = Color.Coral; 
+        Color offColor = Color.Yellow; 
+        int seconds, count; 
+        Button[] Buttons = new Button[10]; //儲存按鈕的陣列(陣列第一個元素沒有用到)
+        int[] btnState = new int[10];   //紀錄每個按鈕的狀態(陣列第一個元素沒有用到)
+        int[,] ChangeCells = {{-1,-1,-1,-1,-1},  //第一列是無用的數值
+                                    {1, 2, 4, 5, -1},  //按鈕1影響按鈕1, 2, 4, 5
+	                   {2, 1, 3, -1, -1}, //按鈕2影響按鈕2, 1, 3
+	                   {3, 2, 5, 6, -1},  //按鈕3影響按鈕3, 2, 5, 6
+	                   {4, 1, 7, -1, -1}, //按鈕4影響按鈕4, 1, 7
+	                   {5, 2, 4, 6, 8},   //按鈕5影響按鈕5, 2, 4, 6, 8
+	                   {6, 3, 9, -1, -1}, //按鈕6影響按鈕6, 3, 9
+	                   {7, 4, 5, 8, -1},  //按鈕7影響按鈕7, 4, 5, 8
+	                   {8, 7, 9, -1, -1}, //按鈕8影響按鈕8, 7, 9
+	                   {9, 5, 6, 8, -1},}; //按鈕9影響按鈕9, 5, 6, 8
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            seconds++; //紀錄的秒數加1
+            lblTime.Text = "秒數:" +seconds.ToString() + "秒"; //顯示目前經過的秒數
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            seconds = count = 0; //將秒數與按鍵次數都重設為0
+            lblCount.Text = "次數:" +count.ToString() + "次"; //顯示按下的次數
+            lblTime.Text = "秒數:"+seconds.ToString() + "秒"; //顯示經過的秒數
+            Random rn = new Random(); //建立亂數產生器
+            for (int i = 1; i <= 9; i++)
+            {
+                Buttons[i].Enabled = true; //將每個數字鍵設成可用狀態
+                btnState[i] = rn.Next(0, 2); //隨機設定每個按鍵的狀態 (0代表關，1代表開)
+                if (btnState[i] == 1) Buttons[i].BackColor = onColor; //若按鍵狀態為開則按鍵顯示咖啡色
+                else Buttons[i].BackColor = offColor; //若按鍵狀態為開則按鍵顯示黃色
+            }
+            timer1.Enabled = true; //開始計時
+            btnStart.Enabled = false; //將開始鍵無效化
+            btnEnd.Enabled = true; //有效化結束鍵
+
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true; //有效化開始鍵
+            btnEnd.Enabled = false; //無效化結束鍵
+            timer1.Enabled = false; //停止計時器
+            for (int i = 1; i <= 9; i++)
+            {
+                Buttons[i].BackColor = Color.LightGray; //將所有按鍵的顏色設為淡灰色
+                Buttons[i].Enabled = false; //無效化所有的數字鍵
+            }
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button btnHit = (Button)sender; //取得目前按鍵的物件實體
+            int Num = int.Parse(btnHit.Text);  //根據按鈕上的數字判定是第幾個按鍵
+            for (int i = 0; i < 5; i++)  //檢查與此按鍵有關的其他按鍵
+            {
+                int X = ChangeCells[Num, i]; //從ChangeCells陣列取得按鍵編號
+                if (X > 0) //若按鍵編號大於0，表示是正確的按鍵編號
+                {
+                    if (btnState[X] == 1) //若按鍵狀態為1
+                    {
+                        btnState[X] = 0; Buttons[X].BackColor = offColor; //將按鍵狀態反相，並顛倒按鍵顏色
+                    }
+                    else
+                    {
+                        btnState[X] = 1; Buttons[X].BackColor = onColor; //將按鍵狀態反相，並顛倒按鍵顏色
+                    }
+                }
+            }
+            count++; //按鍵次數加1
+            lblCount.Text = "次數:" +count.ToString() + "次"; //顯示目前總共按了幾次按鍵
+                                                           // 判別是否達到過關條件
+            int sum = 0;
+            for (int i = 1; i <= 9; i++) sum += btnState[i]; //將所有按鍵的狀態數值加起來
+            if (sum == 8 && btnState[5] == 0) //若按鍵數值總和為8同時按鍵5的狀態值為0，表示已達過關條件
+            {
+                btnStart.Enabled = true; //有效化開始鍵
+                btnEnd.Enabled = false; //無效化結束鍵
+                timer1.Enabled = false; //停止計時器
+                MessageBox.Show("恭喜你過關了!", "九宮格遊戲"); //顯示過關訊息
+                for (int i = 1; i <= 9; i++) Buttons[i].Enabled = false; //無效化所有的數字按鍵
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //將建立的按鈕依序存在按鈕陣列中
+            Buttons[1] = button1;
+            Buttons[2] = button2;
+            Buttons[3] = button3;
+            Buttons[4] = button4;
+            Buttons[5] = button5;
+            Buttons[6] = button6;
+            Buttons[7] = button7;
+            Buttons[8] = button8;
+            Buttons[9] = button9;
+
+            for (int i = 1; i <= 9; i++)
+                Buttons[i].Enabled = false; //讓程式剛啟動時，所有的數字鈕都處於無效的狀態
+            btnStart.Enabled = true; //開始鈕可正常運作
+            btnEnd.Enabled = false; //結束紐處於無效狀態
+
+        }
+    }
+}
